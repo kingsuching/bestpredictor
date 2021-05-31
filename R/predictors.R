e4c999eeaec93c4a6aa72b2287e0b26c19ee5777
kingsuching <sucheen.sundaram@gmail.com>
2021-05-31 11:36:58 -0700

    does not work

--- R/predictors.R
+++ R/predictors.R
@@ -1,1 +1,11 @@
-#' @author Sucheen Sundaram
+#' Generates the predictors
+#'
+#' @import tidyverse
+#' @import dplyr
+#' @export predictors
+
+predictors <- function(df, response) {
+  if(!is.numeric(response)) {
+    stop("Dumb A$$")
+  }
+}

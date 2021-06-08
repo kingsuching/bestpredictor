41979e241a1a74ed2dd9269dfa4e9d10fa4ceca1
kingsuching <sucheen.sundaram@gmail.com>
2021-06-08 15:29:12 -0700

    All-in-One

--- R/predictors.R
+++ R/predictors.R
@@ -7,8 +7,14 @@
 #' @export predictors
 
 predictors <- function(df, response) {
-  models <- data.frame(create_models(df, response))
-  return(models)
+  df <- df %>%
+    select_if(is.numeric)
+  for(i in names(df)) {
+    f <- paste(response, "~", i)
+    m <- lm(formula = f, data = sf_salaries) %>% anova() %>% pluck("Sum Sq")
+    final <- append(final, m)
+  }
+  max_r2 <- max(final)
+  index <- which(max_r2 %in% final)
+  return(names(df)[index])
 }
-
-predictors(sf_salaries, "TotalPay")

a5fdae3504828fe9ccdb326c59ac638d5ec98862
kingsuching <sucheen.sundaram@gmail.com>
2021-06-09 14:42:56 -0700

    Finished

--- R/predictors.R
+++ R/predictors.R
@@ -15,11 +15,11 @@ predictors <- function(df, response) {
   for(i in names(df)) {
     if(i != response) {
       f <- paste(response, "~", i)
-      m <- lm(formula = f, data = sf_salaries) %>% anova() %>% pluck("Sum Sq")
+      m <- lm(formula = f, data = df) %>% anova() %>% pluck("Sum Sq")
       final <- append(final, m[1])
     }
   }
   max_index <- index(final, max(final))
   value <- names(df)[max_index]
-  return(value)
+  return(c(value, max(final)))
 }
